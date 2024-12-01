ea4b3152f5c1b98909a36524b89380efad8be855
shrikant-d5456 <dalvishrikant5456@gmail.com>
2024-12-01 10:24:38 +0530

    upload

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Img from '../assets/shreeImg.jpg'
 
 const aboutData = {
     name: "Shrikant Dalvi",
     desc: "I am a dedicated and passionate frontend developer with a strong foundation in creating user-centric and visually appealing web applications. Currently, I am pursuing a BTech in Information Technology at DBATU University, where I am in my fourth year of study. In addition to my academic pursuits, I am actively engaged in an internship as a frontend developer, working on the Scholarship Management System project. My responsibilities include designing Figma layouts, creating website interfaces, and developing pages using ReactJS.",
     role: "Frontend Developer + Designer",
-    img: "https://via.placeholder.com/150"
+    img: Img
 }
 
 const About = () => {
@@ -17,11 +18,11 @@ const About = () => {
             <div className="relative  md:bg-gradient-to-r md:from-blue-500 md:via-purple-500 md:to-pink-500 p-1 rounded-lg">
                 <div className="bg-white shadow-lg rounded-lg lg:p-4 md:p-4 p-1 lg:max-w-6xl md:max-w-6xl lg:flex md:flex items-center">
                     {/* Logo Section */}
-                    <div className="lg:w-1/3 md:w-1/3 my-4 flex justify-center">
+                    <div className="lg:w-1/3 md:w-1/3 my-4 rotate-45 md:bg-gradient-to-r md:from-blue-500 md:via-purple-500 md:to-pink-500  flex justify-center rounded-full">
                         <img
                             src={aboutData.img}
                             alt="Logo"
-                            className="logoimg w-60 h-60 object-cover "
+                            className="w-60 h-60 object-cover flex rotate-[315deg] rounded-full border-white border-8"
                         />
                     </div>
                     {/* Info Section */}
